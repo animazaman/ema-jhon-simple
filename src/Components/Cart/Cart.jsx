af0a97af26f05f8243973c339120d23da950b848
animazaman <animazaman17@gmail.com>
2023-04-07 12:04:48 +0600

    Saved cart to local storage.

---
+++ src/Components/Cart/Cart.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = (props) => {
+    //const cart = props.cart;
+    const {cart} = props;
+
+    let total =0;
+    let totalShipping = 0;
+    for(const product of cart){
+        total = total+product.price;
+        totalShipping = totalShipping+ product.shipping;
+    }
+
+    const tax = total*7/100;
+    const grandTotal= total+totalShipping+tax;
+
+    return (
+        <div className='cart'>
+            <h4>Order Summary</h4>
+            <p>Selected Items: {cart.length}</p>
+            <p>Total Price:${total}</p>
+            <p>Total Shipping: ${totalShipping}</p>
+            <p>Tax: ${tax}</p>
+            <h2>Grand Total: ${grandTotal} </h2>
+
+        </div>
+    );
+};
+
+export default Cart;
